f0de90821f0087f26d065224ff444de52a20a485
Dylan Palmboom <nofxboy1234@gmail.com>
2023-07-05 10:23:50 +0200

    Update tests for Gear

--- practice/gear.rb
+++ practice/gear.rb
@@ -11,8 +11,11 @@ class Gear
   # Sends a private message to itself (ration)
   # Sends a message to a collaborating object (wheel.diameter) -> Wheel
 
-  # Incoming Query message: changes nothing (no side effects) but does a calculation and returns a result
-  # Test Incoming Query messages by making assertions about what they send back
+  # Incoming Query message: changes nothing (no side effects) but does a calculation and returns a result.
+  # Test Incoming Query messages by making assertions about what they send back.
+  # Test the interface, not the implementation.
+  # If I test only the interface, it means I can change the implementation,
+  # without breaking the test.
   def gear_inches
     ratio * wheel.diameter
   end
--- practice/wheel.rb
+++ practice/wheel.rb
@@ -7,8 +7,11 @@ class Wheel
     @tire = tire
   end
 
-  # Incoming Query message: changes nothing (no side effects) but does a calculation and returns a result
-  # Test Incoming Query messages by making assertions about what they send back
+  # Incoming Query message: changes nothing (no side effects) but does a calculation and returns a result.
+  # Test Incoming Query messages by making assertions about what they send back.
+  # Test the interface, not the implementation.
+  # If I test only the interface, it means I can change the implementation,
+  # without breaking the test.
   def diameter
     rim + (tire * 2)
   end
