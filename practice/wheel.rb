1cdaeea04219aac3e4bfdd63bef43c14774d8ed7
Dylan Palmboom <nofxboy1234@gmail.com>
2023-07-05 09:44:27 +0200

    Update tests for Wheel

--- practice/wheel.rb
+++ practice/wheel.rb
@@ -7,7 +7,8 @@ class Wheel
     @tire = tire
   end
 
-  # incoming query: changes nothing but does a calculation and returns a result
+  # Incoming Query message: changes nothing but does a calculation and returns a result
+  # Test Incoming Query messages by making assertions about what they send back
   def diameter
     rim + (tire * 2)
   end
