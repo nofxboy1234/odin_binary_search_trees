69c908a40123f233419a3390ce213d762a92b018
Dylan Palmboom <nofxboy1234@gmail.com>
2023-07-18 10:49:14 +0200

    Refactor Tree#find

--- lib/tree.rb
+++ lib/tree.rb
@@ -128,8 +128,9 @@ class Tree
   private
 
   def find_recursive(root, value)
-    return if root.nil?
-    return root if root.data == value
+    # return if root.nil?
+    # return root if root.data == value
+    return root if root.nil? || root.data == value
 
     if value < root.data
       find_recursive(root.left, value)
