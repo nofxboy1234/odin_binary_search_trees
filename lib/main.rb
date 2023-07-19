feda9f5b260eb60b23362c82f22695149e7710ff
Dylan Palmboom <nofxboy1234@gmail.com>
2023-07-19 21:15:21 +0200

    Update main.rb driver script

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,30 @@
 require_relative 'tree'
 
+# tree = Tree.new
 tree = Tree.new(array: Array.new(15) { rand(1..100) })
-# puts tree
-# puts tree.root
-# tree.pretty_print(tree.instance_variable_get(:@root))
+tree.pretty_print(tree.root)
 p tree.balanced?
+
+p tree.level_order_recursive
+p tree.level_order_iterative
+puts "\n"
+p tree.preorder
+p tree.inorder
+p tree.postorder
+puts "\n"
+tree.insert(101)
+tree.insert(102)
+tree.insert(103)
+tree.pretty_print(tree.root)
+p tree.balanced?
+tree.rebalance
+tree.pretty_print(tree.root)
+p tree.balanced?
+
+puts "\n"
+p tree.level_order_recursive
+p tree.level_order_iterative
+puts "\n"
+p tree.preorder
+p tree.inorder
+p tree.postorder
